1bf87660a8f347df459a5fc7812b0254f5a600c1
Roger C Guilherme <rogercguilherme@gmail.com>
2025-10-06 02:39:14 -0300

    Modifies the logic of the buttons in the records. Now you no longer need a .pft file.

--- www/htdocs/opac/classes/ToolButtons.php
+++ www/htdocs/opac/classes/ToolButtons.php
@@ -1,45 +1,98 @@
 <?php
-
-class ToolButtons {
-    private $text;
-    private $icon;
-    private $style;
-    private $onclick;
-
-    public function __construct($text, $icon, $style,$onclick) {
-        $this->text = $text;
-        $this->icon = $icon;
-        $this->style = $style;
-        $this->onclick = $onclick;
+/*
+* ToolButtons
+*
+* @author Roger C. Guilherme
+* @date 2025-10-05
+* @description Class to generate tool buttons (Print, Email, Reserve, Download, Word) in the display of records.
+* Now with Record_Toolbar.tab file support for personalized configuration.
+*/
+class ToolButtons
+{
+    var $buttons = array();
+    function __construct($buttons)
+    {
+        $this->buttons = $buttons;
     }
 
-    public function render() {
-        $button = '<button class="btn m-1 btn-' . $this->style . '" onclick="'.$this->onclick.'">';
-        $button .= '<i class="fa fa-' . $this->icon . '"></i> <span class="d-none d-sm-block"> ' . $this->text;
-        $button .= '</span></button>';
-        return $button;
+    function Show()
+    {
+        $html = "";
+        if (isset($this->buttons) and count($this->buttons) > 0) {
+            foreach ($this->buttons as $key => $value) {
+                $linkjs = 'javascript:SendTo("reserve_one","c_=\',mstname,\'_=\'f(mfn,1,0)\'")';
+                if ($value == "Y") {
+                    if ($key == "print")     $html .= " <a href=\"javascript:void(0)\" onclick=\"print()\" class=\"btn btn-light\" title=\"" . msg("print") . "\"><i class=\"fas fa-print\"></i></a>\n";
+                    if ($key == "iso")       $html .= " <a href=\"javascript:void(0)\" onclick=\"download()\" class=\"btn btn-light\" title=\"" . msg("download") . " ISO\"><i class=\"fas fa-download\"></i></a>\n";
+                    if ($key == "word")      $html .= " <a href=\"". $linkjs."\" onclick=\"ms_word()\" class=\"btn btn-light\" title=\"MS Word\"><i class=\"fas fa-file-word\"></i></a>\n";
+                    if ($key == "email")     $html .= " <a href=\"javascript:void(0)\" onclick=\"email()\" class=\"btn btn-light\" title=\"" . msg("send_email") . "\"><i class=\"fas fa-envelope\"></i></a>\n";
+                    if ($key == "reserve")   $html .= " <a href=\"javascript:void(0)\" onclick=\"reserve()\" class=\"btn btn-light\" title=\"" . msg("reserve") . "\"><i class=\"fas fa-book\"></i></a>\n";
+                }
+            }
+        }
+        return $html;
     }
 
-    public function rendercard() {
-        $button = '<a class="btn btn-outline-' . $this->style . '" href='.$this->onclick.'>';
-        $button .= '<i class="fa fa-' . $this->icon . '"></i> <span class="d-none d-sm-inline" alt="'.$this->text.'"> ';
-        $button .= '</a>';
-        return $button;
+    /**
+     * New function to generate the buttons HTML based on the Record_Toolbar.tab file
+     *
+     * @param string $db_path - Database path
+     * @param string $base    - Selected database name
+     * @param string $lang    - Current language
+     * @return string         - Returns only the html of the <a> tags or an empty string
+     */
+    function ShowFromTab($db_path, $base, $lang)
+    {
+        global $lang;
+        $buttons_file_db = $db_path . $base . '/opac/' . $lang . '/' . '/record_toolbar.tab';
+        $buttons_file_meta = $db_path . "opac_conf/" . $lang . "/record_toolbar.tab";
+
+        $buttons_file_path = "";
+        if (file_exists($buttons_file_db)) {
+            $buttons_file_path = $buttons_file_db;
+        } elseif (file_exists($buttons_file_meta)) {
+            $buttons_file_path = $buttons_file_meta;
+        }
+
+        if ($buttons_file_path === "") {
+            return "";
+        }
+
+        $buttons_html = ""; 
+        $lines = file($buttons_file_path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+        foreach ($lines as $line) {
+            $parts = explode('|', $line);
+
+            if (count($parts) < 4) continue;
+
+            $param   = trim($parts[0]);
+            $enabled = trim($parts[1]);
+            $icon    = trim($parts[2]);
+            $alt     = trim($parts[3]);
+
+            if (strtoupper($enabled) !== 'Y') {
+                continue;
+            }
+
+            if ($param === 'permalink') {
+                if (isset($parts[4]) && !empty(trim($parts[4]))) {
+                    $field_tag = trim($parts[4]);
+                   
+                    $buttons_html .= '<button type="button" class="btn btn-light" title="' . htmlspecialchars($alt) . '" ' .
+                        'data-base="' . htmlspecialchars($base) . '" ' .
+                        'data-k="\'' . htmlspecialchars($field_tag) . '\'" ' .
+                        'onclick="showPermalinkModal(this)">' .
+                        '<i class="fas fa-' . htmlspecialchars($icon) . '"></i>' .
+                        '</button>' . "\n";
+                }
+            } else {
+                $js_function = str_replace('-', '_', $param);
+                $linkjs = 'javascript:SendTo("'.$js_function.'","c_=\',mstname,\'_=\'f(mfn,1,0)\'")';
+                
+                $buttons_html .= ' <a href='.$linkjs.'  class="btn btn-light" title="' . htmlspecialchars($alt) . '"><i class="fas fa-' . htmlspecialchars($icon) . '"></i></a>' . "\n";
+            }
+        }
+        return $buttons_html; 
     }
 }
-
-    $printButton = new ToolButtons('Imprimir', 'print', 'secondary','SendToPrint()');
-    $downloadButton = new ToolButtons('Baixar ISO', 'download', 'success','javascript:SendTo("iso","c_=\',mstname,\'_=\'f(mfn,1,0)\'")');
-    $WordButton = new ToolButtons('MS Word', 'file-word', 'primary','SendToWord()');
-    $emailButton = new ToolButtons('Enviar por email', 'envelope', 'info','ShowHide(\'myMail\')');
-    $reserveButton = new ToolButtons('Reserve', 'book', 'success','ShowHide(\'myReserve\')');
-    $backButton = new ToolButtons('Voltar', 'arrow-alt-circle-left', 'light','document.regresar.submit()');
-
-    $printCardButton = new ToolButtons('Imprimir','print','secondary','javascript:SendTo("print_one","c_=\',mstname,\'_=\'f(mfn,1,0)\'")');
-    $downloadCardButton = new ToolButtons('Baixar ISO', 'download', 'success','javascript:SendToISO()');
-    $WordCardButton = new ToolButtons('MS Word', 'file-word', 'primary','javascript:SendTo("word","c_=\',mstname,\'_=\'f(mfn,1,0)\'")');
-    $emailCardButton = new ToolButtons('Enviar por email', 'envelope', 'info','javascript:SendTo("mail_one","c_=\',mstname,\'_=\'f(mfn,1,0)\'")');
-    $reserveCardButton = new ToolButtons('Reserve', 'book', 'success','javascript:SendTo("reserve_one","c_=\',mstname,\'_=\'f(mfn,1,0)\'")');
-    $xmlCardButton = new ToolButtons('Voltar', 'code', 'primary','javascript:SendToXML("c_=\',mstname,\'_=\'f(mfn,1,0)\'")');
-
-?>
