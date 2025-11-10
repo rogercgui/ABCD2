bb9644974f21b5c4cf8e357d18a0234380b88630
Roger C Guilherme <rogercguilherme@gmail.com>
2025-11-10 01:54:55 -0300

    Improves the functioning of the dropdown menu next to the search box, now allowing you to choose the database or collection.

--- www/htdocs/opac/views/dropdown_db.php
+++ www/htdocs/opac/views/dropdown_db.php
@@ -1,17 +1,76 @@
 <?php
-
 /**
- * This script displays a dropdown with the list of available databases.
- * 20230312 rogercgui Created
+ * -------------------------------------------------------------------------
+ *  ABCD - Automação de Bibliotecas e Centros de Documentação
+ *  https://github.com/ABCD-DEVCOM/ABCD
+ * -------------------------------------------------------------------------
+ *  Script:   dropdown_db.php
+ *  Purpose:  Displays a dropdown menu for selecting databases/collections in the OPAC.
+ *  Author:   Roger C. Guilherme
+ *
+ *  Changelog:
+ *  -----------------------------------------------------------------------
+ *  2023-03-12 rogercgui Created
+ *  2025-11-09 rogercgui Added logic to pre-select the base/collection from the $base (URL) parameter.
+ * -------------------------------------------------------------------------
  */
+
+// 1. Determine the button text and active selection BEFORE drawing the HTML.
+//    This is necessary because the button is drawn before the loop containing
+//    the collection names.
+
+		$selected_text = $msgstr["front_catalog"]; // Valor padrão
+$is_default_active = "active"; // "Catálogo" é ativo por padrão
+
+$current_base_value = $base ?? ($_REQUEST["base"] ?? "");
+
+if ($current_base_value != "") {
+	if (isset($bd_list[$current_base_value])) {
+		// É uma base (ex: "marc")
+		$selected_text = $bd_list[$current_base_value]["titulo"];
+		$is_default_active = ""; // O padrão "Catálogo" não é mais ativo
+	} elseif (strpos($current_base_value, 'col:') === 0) {
+		// É uma coleção (ex: "col:C[marc]"). Precisamos procurá-la.
+		$found_col = false;
+		foreach ($bd_list as $key_db => $value_db) {
+			$archivo_col = $db_path . $key_db . "/opac/" . $lang . "/" . $key_db . "_colecciones.tab";
+			if (file_exists($archivo_col)) {
+				$fp_col = file($archivo_col);
+				foreach ($fp_col as $colec_line) {
+					$colec_line = trim($colec_line);
+					if ($colec_line != "") {
+						$v_col = explode('|', $colec_line);
+						// Recria o valor da coleção
+						$col_val_check = "col:" . (isset($v_col[2]) ? $v_col[2] . $v_col[0] : $v_col[0]);
+						if ($col_val_check == $current_base_value) {
+							$selected_text = $v_col[1]; // Achou!
+							$is_default_active = ""; // O padrão "Catálogo" não é mais ativo
+							$found_col = true;
+							break;
+						}
+					}
+				}
+			}
+			if ($found_col) break;
+		}
+	}
+}
 ?>
 
 <div class="dropdown">
 	<button class="btn btn-light dropdown-toggle w-100" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-		<?php echo $msgstr["front_catalog"]; ?>
+		<?php echo htmlspecialchars($selected_text); // 2. Usar o texto pré-selecionado 
+		?>
 	</button>
 	<ul class="dropdown-menu w-100">
-
+		<li>
+			<a class="dropdown-item dropdown-item-select <?php echo $is_default_active; ?>" href="#" data-value="" data-text="<?php echo $msgstr['front_catalog']; ?>">
+				<?php echo $msgstr["front_catalog"]; ?>
+			</a>
+		</li>
+		<li>
+			<hr class="dropdown-divider">
+		</li>
 		<?php
 		if (!isset($_REQUEST["existencias"]) or trim($_REQUEST["existencias"]) == "") {
 
@@ -41,8 +100,13 @@
 					echo "**" . $value_info . "<br>";
 				}
 				if (trim($value["nombre"]) != "") {
-					// Usando o cabeçalho de dropdown para o nome da base de dados
-					echo "<li><h5 class=\"dropdown-header\"><a class='dropdown-item' href='javascript:BuscarIntegrada(\"$key\",\"\",\"free\",\"\",\"\",\"\",\"\",\"\",\"\",\"$home_link\")'>" . $value["titulo"] . "</a></h5></li>\n";
+
+					// 4. Controlar a classe 'active' da Base
+					$is_base_active = ($current_base_value == $key) ? 'active' : '';
+					echo "<li>";
+					echo "<a class='dropdown-item dropdown-item-select fw-bold " . $is_base_active . "' href='#' data-value='" . htmlspecialchars($key) . "' data-text='" . htmlspecialchars($value["titulo"]) . "'>" . $value["titulo"] . "</a>";
+					echo "</li>\n";
+
 					if (file_exists($archivo)) {
 						$fp = file($archivo);
 						foreach ($fp as $colec) {
@@ -56,9 +120,13 @@
 										$cipar = "_" . strtolower($v[0]);
 									else
 										$cipar = "";
+									$col_value = "col:" . (isset($v[2]) ? $v[2] . $v[0] : $v[0]); // Gera "col:..."
+
+									// 5. Controlar a classe 'active' da Coleção
+									$is_col_active = ($current_base_value == $col_value) ? 'active' : '';
 									echo "<li>";
-									echo "<a class='dropdown-item' href='javascript:BuscarIntegrada(\"$key\",\"1B\",\"free\",\"$col_expr\",\"$colec\",\"\",\"\",\"\",\"\",\"\")'>" . $v[1] . "</a>";
-									echo "</li>";
+									echo "<a class='dropdown-item dropdown-item-select ps-4 " . $is_col_active . "' href='#' data-value='" . htmlspecialchars($col_value) . "' data-text='" . htmlspecialchars($v[1]) . "'>" . $v[1] . "</a>";
+									echo "</li>\n";
 								}
 							}
 						}
