6e3cdfd4f1ed2f1d7d054bf5d23da70fed620b57
Roger C Guilherme <rogercguilherme@gmail.com>
2025-11-10 01:32:59 -0300

    Adds the header and improves the HTML.

--- www/htdocs/opac/index.php
+++ www/htdocs/opac/index.php
@@ -1,32 +1,38 @@
 <?php
-/**************** Modifications ****************
-2022-03-23 rogercgui change the folder /par to the variable $actparfolder
-***********************************************/
+/**
+ * -------------------------------------------------------------------------
+ *  ABCD - Automated Library Management System
+ *  https://github.com/ABCD-DEVCOM/ABCD
+ * -------------------------------------------------------------------------
+ *  Script:   www/htdocs/opac/index.php
+ *  Purpose:  Main entry point for the OPAC interface
+ *  Author:   Roger C. Guilherme
+ *
+ *  Changelog:
+ *  -----------------------------------------------------------------------
+ *  2022-03-23 rogercgui change the folder /par to the variable $actparfolder
+ * -------------------------------------------------------------------------
+ */
+
+
+// --- 1. CONFIGURAÇÃO E INCLUDES ESSENCIAIS ---
+// Define o $contexto_page para o sidebar saber que estamos na busca
+$contexto_page = "busca";
+
 include realpath(__DIR__ . '/../central/config_opac.php');
-
-$primeraPagina="Y";
-
-
-if (isset($_REQUEST["primeravez"]) and $_REQUEST["primeravez"]=="Y"){ ?>
-<script>
-	document.cookie = 'ABCD=;';
-</script>
-
-<?php 
-}
 
 include("head.php");
 
 if (isset($_REQUEST["cookie"])) {
 	include("views/view_selection.php");
-}  elseif (isset($_REQUEST["k"])) {
+} elseif (isset($_REQUEST["k"])) {
 	include("components/permalink.php");
-}  elseif ((isset($_REQUEST["indice"])) and  $_REQUEST["indice"]==="yes" ){
-	$startpage="N";
+} elseif ((isset($_REQUEST["indice"])) and  $_REQUEST["indice"] === "yes") {
+	$startpage = "N";
 	include("views/alfabetico.php");
 } else {
 	include("views/content_home.php");
 }
 ?>
 
-<?php include("views/footer.php");?>
+<?php include("views/footer.php"); ?>
