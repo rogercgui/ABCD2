61dd2cbf0dbc4160e0d1e6787e926b0b338d0b57
Roger C Guilherme <rogercguilherme@gmail.com>
2025-11-18 11:09:03 -0300

    Allows you to hide the dropdown menu of the free search form.

--- www/htdocs/opac/components/search_free.php
+++ www/htdocs/opac/components/search_free.php
@@ -1,8 +1,8 @@
-<!------------Inicio do search_free.php---------------->
 <?php
 /*
 2022-03-07 rogercgui fixed index $prefijo=$x[1];
 2025-09-28 rogercgui added check if indice.ix has fields with columnas to show button
+2025-11-18 rogercgui corrected to use $hide_filter variable
 */
 ?>
 
@@ -51,14 +51,20 @@ if (!isset($mostrar_libre) || $mostrar_libre != "N") {
 			if ($base != "") echo "<input id=base type=hidden name=base value=" . $base . ">\n";
 			if (isset($_REQUEST["modo"])) echo "<input type=hidden name=modo value=" . $_REQUEST["modo"] . ">\n";
 			if (isset($_REQUEST['Sub_Expresion'])) $_REQUEST['Sub_Expresion'] = urldecode(str_replace('~', '', $_REQUEST['Sub_Expresion']));
+
+			if ($hide_filter == "Y") $col_md="col-md-6"; else $col_md="col-md-9";
 			?>
 			<div class="row g-3">
-				<div class="col-md-6">
+				<div class="<?php echo $col_md;?>">
 					<input class="form-control" type="text" name="Sub_Expresion" id="termo-busca" value="<?php if (isset($_REQUEST['Sub_Expresion'])) echo htmlentities($_REQUEST['Sub_Expresion']); ?>" placeholder="<?php echo $msgstr["front_search"] ?>  ..." />
 				</div>
+
+				<?php if ($hide_filter=="Y") { ?>
 				<div class="col-md-3">
 					<?php include $Web_Dir . 'views/dropdown_db.php'; ?>
 				</div>
+				<?php } ?>
+
 				<div class="col-md-3">
 					<button id="submit-busca-livre" type="submit" class="btn btn-success btn-submit mb-3 w-100"><i class="fa fa-search"></i> <?php echo $msgstr["front_search"] ?></button>
 				</div>
@@ -82,12 +88,12 @@ if (!isset($mostrar_libre) || $mostrar_libre != "N") {
 			<div class="row g-3 py-2">
 				<?php
 				if (!isset($_REQUEST["submenu"]) || $_REQUEST["submenu"] != "N") {
-					$archivo = "";
+					$archivo_ix = "";
 					if (isset($_REQUEST["modo"])) {
 						if ($_REQUEST["modo"] == "integrado") {
-							$archivo = $db_path . "/opac_conf/" . $lang . "/indice.ix";
+							$archivo_ix = $db_path . "/opac_conf/" . $lang . "/indice.ix";
 						} elseif ($base != "") {
-							$archivo = $db_path . $base . "/opac/" . $lang . "/" . $base . ".ix";
+							$archivo_ix = $db_path . $base . "/opac/" . $lang . "/" . $base . ".ix";
 						}
 					}
 
@@ -95,8 +101,8 @@ if (!isset($mostrar_libre) || $mostrar_libre != "N") {
 					$mostrar_botao_indice = false;
 
 					// 2. Verifica se o arquivo existe antes de tentar lê-lo
-					if (file_exists($archivo)) {
-						$fp_check = file($archivo);
+					if (file_exists($archivo_ix)) {
+						$fp_check = file($archivo_ix);
 						foreach ($fp_check as $value_check) {
 							$val_check = trim($value_check);
 							if ($val_check != "") {
@@ -127,9 +133,9 @@ if (!isset($mostrar_libre) || $mostrar_libre != "N") {
 
 <?php if (!isset($_REQUEST["submenu"]) || $_REQUEST["submenu"] != "N") { ?>
 	<div style="clear:both;"></div>
-	<div id="sub_menu" style="display: none;" name="sub_menu">
-		<ul>
+	<div id="sub_menu" style="display: none;" name="sub_menu" class="mt-2">
 			<?php
+
 			if ($multiplesBases == "Y" && $base != "") {
 				$dbname = $base;
 			} else {
@@ -163,7 +169,7 @@ if (!isset($mostrar_libre) || $mostrar_libre != "N") {
 						if (isset($v[2])) { // Adiciona verificação para evitar erro se a coluna não existir
 							$columnas = $v[2];
 							if ($columnas >= 1)
-								echo "<li><a href='Javascript:ActivarIndice(\"" . str_replace("'", "", $v[0]) . "\",$columnas,\"inicio\",90,1,\"" . $v[1] . "\",\"" . "$base\")'>" . $v[0] . "</a></li>\n";
+								echo "<a href='Javascript:ActivarIndice(\"" . str_replace("'", "", $v[0]) . "\",\"inicio\",90,1,\"" . $v[1] . "\",\"" . "$base\")'  class=\"btn btn-outline-primary btn-sm m-1\" >" . $v[0] . "</a>\n";
 						}
 					}
 				}
@@ -187,7 +193,6 @@ if (!isset($mostrar_libre) || $mostrar_libre != "N") {
 				}
 			}
 			?>
-		</ul>
 	</div>
 	<input type="hidden" name="Opcion" value="libre">
 	<input type="hidden" name="prefijo" value="<?php echo $prefijo; ?>">
