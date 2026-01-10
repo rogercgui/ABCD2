dabb839044542b5e21901dee441a0db361fd179c
Fred <64411779+fho4abcd@users.noreply.github.com>
2026-01-10 14:13:59 +0100

    Update version to v3.1.2 with new date

--- www/htdocs/version.php
+++ www/htdocs/version.php
@@ -2,5 +2,5 @@
 // This file defines the current version of the software.
 // It is the only place that needs to be changed with each new version.
 
-define('ABCD_VERSION', 'v3.1.1');
-define('DATE_VERSION', '2025-12-23');
+define('ABCD_VERSION', 'v3.1.2');
+define('DATE_VERSION', '2026-01-10');
